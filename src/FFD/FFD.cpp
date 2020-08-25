ca4835f4c5d24bb344e2c2be77379da0d61fba16
WhiteBeerHouse <1335524854@qq.com>
2020-08-26 03:03:21 +0800

    #15 Reorganize

---
+++ src/FFD/FFD.cpp
@@ -0,0 +1,55 @@
+#include <time.h>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> items;
+int n, c;
+
+bool cmp(int item1, int item2){
+	return item1 > item2;
+}
+
+int first_fit_decreasing(){
+	sort(items.begin(), items.end(), cmp);
+	vector<int> bins;
+	for (int i = 0; i < n; ++i){
+		int j;
+		for (j = 0; j < bins.size(); ++j){
+			if (items[i] <= bins[j]){
+				bins[j] -= items[i];
+				break;
+			}
+		}
+		//if no opened bin satisfies, allocate a new bin
+		if (j == bins.size()){
+			bins.push_back(c - items[i]);
+		}		
+	}
+	return bins.size();
+}
+
+int main(int argc, char* argv[]) {
+	ifstream file;
+	file.open(argv[1]);
+	if (!file.is_open()){
+		perror("Error");
+		return(-1);
+	}
+	file >> n >> c;
+	items.resize(n);
+	for (int i = 0; i < n; ++i) {
+		file >> items[i];
+	}
+	file.close();
+
+	clock_t start = clock();
+	int solution = first_fit_decreasing();
+	cout << "Solution by first_fit_decreasing: " << solution << endl;
+	clock_t end = clock();
+	cout << fixed << "Time: " << (double)(end - start) / CLOCKS_PER_SEC << endl;
+	return 0;
+}
---
+++ src/NF/NF.cpp
@@ -0,0 +1,44 @@
+#include <time.h>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+vector<int> items;
+int n, c;
+
+int next_fit(){
+	int bins = 0, left = 0;
+	for (int i = 0; i < n; ++i){
+		if (items[i] <= left)
+			left -= items[i];
+		else {
+			++bins;
+			left = c - items[i];
+		}
+	}
+	return bins;
+}
+
+int main(int argc, char* argv[]) {
+	ifstream file;
+	file.open(argv[1]);
+	if (!file.is_open()){
+		perror("Error");
+		return(-1);
+	}
+	file >> n >> c;
+	items.resize(n);
+	for (int i = 0; i < n; ++i) {
+		file >> items[i];
+	}
+	file.close();
+
+	clock_t start = clock();
+	int solution = next_fit();
+	cout << "Solution by next_fit: " << solution << endl;
+	clock_t end = clock();
+	cout << fixed << "Time: " << (double)(end - start) / CLOCKS_PER_SEC << endl;
+	return 0;
+}
